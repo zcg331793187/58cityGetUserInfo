66632a16c93a8faaca5fdf6f37158218c67d0a14
周才广 <zhoucaiguang@gmail.com>
2017-02-21 21:29:15 +0800

    初始化项目

---
+++ typings/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference path="globals/node/index.d.ts" />
+/// <reference path="modules/cheerio/index.d.ts" />
+/// <reference path="modules/express/index.d.ts" />
+/// <reference path="modules/request/index.d.ts" />
